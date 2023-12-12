18d03f437addedd6f26e96ac8d94830d3c7fe59a
Roland <roland.rebek87@gmail.com>
2023-12-12 13:07:39 +0000

    Setup config information updated.

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ except ImportError:
 config = {
     'description' : 'Christmas Theamed codebar project',
     'author' : 'Roland Rebek',
-    'url' : 'URL to get it at.',
-    'download_url' : 'Where t download it.',
+    'url' : 'https://github.com/Roland87/Codebar-Christmas-theamed',
+    'download_url' : 'https://github.com/Roland87/codebar-christmas-project/archive/v0.1.tar.gz',
     'author_email' : 'roland.rebek87@gmail.com',
     'version' : '0.1',
     'install_requires' : ['pytest', 'flask'],
-    'packages' : ['pytest', 'flask'],
+    'packages' : [],
     'scripts' : [],
     'name' : 'Codebar Christmas Project'
 }
--- tests/app_test.py
+++ tests/app_test.py
@@ -1,11 +1,13 @@
+from app import app
 import pytest
-import name
 
-def setup():
-    print("SETUP!")
+def test_home_page():
+    client = app.test_client()
+    response = client.get('/')
+    assert b'Codebar Christmas' in response.data
+    assert b'Season\'s Greetings!' in response.data
 
-def teardown():
-    print("TER DOWN!")
-
-def test_basic():
-    print("I RAN!")
+def test_invalid_route():
+    client = app.test_client()
+    response = client.get('/invalid-route')
+    assert response.status_code == 404
